3a759e1431f2c96877f93247001943c3336c6df3
John Cupitt <jcupitt@gmail.com>
2023-07-18 11:58:04 +0100

    paste in save options create

--- src/saveoptions.c
+++ src/saveoptions.c
@@ -11,7 +11,11 @@ struct _SaveOptions
 	VipsOperation *save_operation;
 
 	// a box we can fill with widgets for the save options
-	GtkWidget *options_box;
+	GtkWidget *options_grid;
+
+	// hash property names to the widghet for that property ... we fetch 
+	// values from here when we make the saver
+	GHashTable *value_widgets;
 };
 
 struct _SaveOptionsClass
@@ -29,6 +33,7 @@ save_options_dispose( GObject *object )
 	printf( "save_options_dispose:\n" ); 
 
 	VIPS_UNREF( options->save_operation );
+	VIPS_FREEF( g_hash_table_destroy, options->value_widgets );
 
 	G_OBJECT_CLASS( save_options_parent_class )->dispose( object );
 }
@@ -54,6 +59,178 @@ save_options_response( GtkWidget *dialog, int response, void *user_data )
 	}
 }
 
+/* This function is used by:
+ *
+ * 	save_options_build_content_box_argument_map_fn_helper
+ *
+ * to process one property of the save operation. The property type and name
+ * are used to create a labelled user input element for that property.
+ */
+static void
+save_options_add_option( SaveOptions *options,
+	GParamSpec *pspec, VipsArgumentClass *argument_class, int *row )
+{
+	/* Get the nickname of the property of the save operation currently
+	 * being processed. For VIPS, this is the user-facing name of the
+	 * property.
+	 */
+	const gchar *name = g_param_spec_get_nick( pspec );
+
+	GType otype = G_PARAM_SPEC_VALUE_TYPE( pspec );
+
+	GtkWidget *t, *label;
+
+	/* For now, skip properties of type VipsImage or VipsObject.
+	 */
+	if( g_type_is_a( otype, VIPS_TYPE_IMAGE ) ||
+		g_type_is_a( otype, VIPS_TYPE_OBJECT ) )
+		return;
+
+	/* Make a widget for this value. The widget
+	 * chosen depends on the type of the property. Set the initial value of
+	 * the user input widget to the default value for the property.
+	 */
+	if( G_IS_PARAM_SPEC_STRING( pspec ) ) {
+		GParamSpecString *pspec_string = G_PARAM_SPEC_STRING( pspec );
+		GtkEntryBuffer* buffer =
+			gtk_entry_buffer_new( pspec_string->default_value, -1 );
+
+		t = gtk_text_new_with_buffer( buffer );
+	}
+	else if( G_IS_PARAM_SPEC_BOOLEAN( pspec ) ) {
+		GParamSpecBoolean *pspec_boolean = G_PARAM_SPEC_BOOLEAN( pspec );
+		t = gtk_check_button_new();
+		gtk_check_button_set_active( GTK_CHECK_BUTTON( t ),
+			pspec_boolean->default_value );
+	}
+	else if( G_IS_PARAM_SPEC_ENUM( pspec ) ) {
+		GParamSpecEnum *pspec_enum = G_PARAM_SPEC_ENUM( pspec );
+		int n_values = pspec_enum->enum_class->n_values - 1;
+		const char **property_nicknames =
+			g_malloc( (n_values + 1) * sizeof( char * ) );
+
+		for( int i = 0; i < n_values; ++i )
+			property_nicknames[i] =
+				pspec_enum->enum_class->values[i].value_nick;
+		property_nicknames[n_values] = NULL;
+
+		t = gtk_drop_down_new_from_strings( property_nicknames );
+		gtk_drop_down_set_selected( GTK_DROP_DOWN( t ),
+			pspec_enum->default_value );
+		// FIXME ... free property_nicknames
+	}
+	else if( G_IS_PARAM_SPEC_INT64( pspec ) ) {
+		GParamSpecInt64 *pspec_int64 = G_PARAM_SPEC_INT64( pspec );
+		t = gtk_spin_button_new_with_range( pspec_int64->minimum,
+			pspec_int64->maximum, 1 );
+
+		gtk_spin_button_set_value( GTK_SPIN_BUTTON( t ),
+			(gint64)pspec_int64->default_value );
+	}
+	else if( G_IS_PARAM_SPEC_INT( pspec )) {
+		GParamSpecInt *pspec_int = G_PARAM_SPEC_INT( pspec );
+		t = gtk_spin_button_new_with_range( pspec_int->minimum,
+			pspec_int->maximum, 1 );
+
+		gtk_spin_button_set_value( GTK_SPIN_BUTTON( t ),
+			(int)pspec_int->default_value );
+	}
+	else if( G_IS_PARAM_SPEC_UINT64( pspec ) ) {
+		GParamSpecUInt64 *pspec_uint64 = G_PARAM_SPEC_UINT64( pspec );
+		t = gtk_spin_button_new_with_range( pspec_uint64->minimum,
+			pspec_uint64->maximum, 1 );
+
+		gtk_spin_button_set_value( GTK_SPIN_BUTTON( t ),
+			(guint64)pspec_uint64->default_value );
+	}
+	else if( G_IS_PARAM_SPEC_DOUBLE( pspec ) ) {
+		GParamSpecDouble *pspec_double = G_PARAM_SPEC_DOUBLE( pspec );
+
+		t = gtk_spin_button_new_with_range( pspec_double->minimum,
+			pspec_double->maximum, 1 );
+
+		gtk_spin_button_set_value( GTK_SPIN_BUTTON( t ),
+			pspec_double->default_value );
+	}
+	else if( G_IS_PARAM_SPEC_BOXED( pspec ) ) {	
+		if( g_type_is_a( otype, VIPS_TYPE_ARRAY_INT ) ) {
+			/* No default values exist for ParamSpecBoxed, so make
+			 * some up for now.
+			 */
+			t = gtk_spin_button_new_with_range( 0, 1000, 1 );
+			gtk_spin_button_set_value( GTK_SPIN_BUTTON( t ), 0 );
+		}
+		else if( g_type_is_a( otype, VIPS_TYPE_ARRAY_DOUBLE ) ) {
+			/* No default values exist for ParamSpecBoxed, so make
+			 * some up for now.
+			 */
+			t = gtk_spin_button_new_with_range( 0, 1000, .1 );
+			gtk_spin_button_set_value( GTK_SPIN_BUTTON( t ), 0 );
+		}
+		else if( g_type_is_a( otype, VIPS_TYPE_ARRAY_IMAGE ) ) {
+			/* Ignore VipsImage-type parameters for now.
+			 */
+			return;
+		}
+		else {
+			/* Ignore parameters of unrecognized type for now.
+			 */
+			return;
+		}
+	}
+	else {
+		printf( "Unknown type for property \"%s\"\n", name );
+		return;
+	}
+
+	printf( "save_options_add_option: adding widget for %s\n", name );
+
+	/* Label for setting, with a tooltip.
+	 */
+	label = gtk_label_new( name );
+	gtk_widget_set_halign( label, GTK_ALIGN_START );
+	gtk_widget_set_tooltip_text( GTK_WIDGET( label ),
+		g_param_spec_get_blurb( pspec ) );
+	gtk_grid_attach( GTK_GRID( options->options_grid ), label, 
+		0, *row, 1, 1 );
+
+	g_object_set( t, "hexpand", true, NULL );
+	gtk_grid_attach( GTK_GRID( options->options_grid ), t, 
+		1, *row, 1, 1 );
+	gtk_widget_set_tooltip_text( GTK_WIDGET( label ),
+		g_param_spec_get_blurb( pspec ) );
+
+	g_hash_table_insert( options->value_widgets, (gpointer) name, t );
+
+	*row += 1;
+}
+
+/* This is the function used by save_options_build_content_box to process a
+ * single property of the save operation.
+ *
+ * See also save_options_build_content_box_argument_map_fn_helper.
+ */
+static void *
+save_options_build_content_box_argument_map_fn( VipsObject *operation,
+	GParamSpec *pspec, VipsArgumentClass *argument_class,
+	VipsArgumentInstance *argument_instance, void *a, void *b )
+{
+	VipsArgumentFlags flags = argument_class->flags;
+	SaveOptions *options = (SaveOptions *) a;
+	int *row = (int *) b;
+
+	/* Include arguments listed in the constructor.
+	 *
+	 * Exclude required (we've set these already) or deprecated arguments.
+	 */
+	if ( !(flags & VIPS_ARGUMENT_DEPRECATED) &&
+		(flags & VIPS_ARGUMENT_CONSTRUCT) &&
+		!(flags & VIPS_ARGUMENT_REQUIRED) )
+		save_options_add_option( options, pspec, argument_class, row );
+
+	return NULL;
+}
+
 static void
 save_options_init( SaveOptions *options )
 {
@@ -61,11 +238,10 @@ save_options_init( SaveOptions *options )
 
 	gtk_widget_init_template( GTK_WIDGET( options ) );
 
-	// walk save_operation and fill options_box with widgets
+	options->value_widgets = g_hash_table_new( g_str_hash, g_str_equal );
 
 	g_signal_connect_object( options, "response", 
 		G_CALLBACK( save_options_response ), options, 0 );
-
 }
 
 #define BIND( field ) \
@@ -83,7 +259,7 @@ save_options_class_init( SaveOptionsClass *class )
 	gtk_widget_class_set_template_from_resource( widget_class,
 		APP_PATH "/saveoptions.ui");
 
-	BIND(options_box);
+	BIND( options_grid );
 }
 
 SaveOptions *
@@ -92,6 +268,7 @@ save_options_new( GtkWindow *parent_window, const char *title,
 {
 	const char *saver;
 	SaveOptions *options;
+	int row;
 
 	if( !(saver = vips_foreign_find_save( filename )) ) {
 		// this will display the error back in the main window, which
@@ -99,6 +276,8 @@ save_options_new( GtkWindow *parent_window, const char *title,
 		//
 		// maybe display the save options, but put the error
 		// message in the dialog?
+		//
+		// could put progress in the dialog too
 		return( NULL );
 	}
 
@@ -114,6 +293,14 @@ save_options_new( GtkWindow *parent_window, const char *title,
 		"in", image,
 		"filename", filename,
 		NULL );
+
+	row = 0;
+	vips_argument_map( VIPS_OBJECT( options->save_operation ),
+		save_options_build_content_box_argument_map_fn,
+		options, &row );
+
+        //<property name="max-content-height">500</property>
+        //<property name="min-content-height">600</property>
 
 	return( options );
 }
